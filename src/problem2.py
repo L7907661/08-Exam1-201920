ab82749ae6f9f516f6d564b3a324befc70675333
L7907661 <hlbtaioe@163.com>
2018-12-11 21:41:59 -0500

    :)

--- src/problem2.py
+++ src/problem2.py
@@ -3,8 +3,8 @@ Exam 1, problem 2.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Zeyu Liao.
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -15,7 +15,7 @@ def main():
 def test_factor_sum():
     """ Tests the   factor_sum   function. """
     ###########################################################################
-    #  TODO: 2. Implement this TEST function, as follows:
+    #  done: 2. Implement this TEST function, as follows:
     #
     #    1. Read the  doc-string of the   factor_sum   function defined below.
     #
@@ -44,37 +44,71 @@ def test_factor_sum():
     ###########################################################################
     # WRITE YOUR TESTS BELOW HERE:
     ###########################################################################
-
+# Test 1:
+    expected = 11
+    actual = factor_sum(28)
+    print('Test 1:')
+    print('Expected:', expected)
+    print('Actual:  ', actual)
+# Test 2:
+    expected = 4
+    actual = factor_sum(25)
+    print('Test 2:')
+    print('Expected:', expected)
+    print('Actual:  ', actual)
+# Test 3:
+    expected = 4
+    actual = factor_sum(28)
+    print('Test 3:')
+    print('Expected:', expected)
+    print('Actual:  ', actual)
+# Test 4:
+    expected = 2
+    actual = factor_sum(2)
+    print('Test 4:')
+    print('Expected:', expected)
+    print('Actual:  ', actual)
+# Test 5:
+    expected = 1
+    actual = factor_sum(3)
+    print('Test 5:')
+    print('Expected:', expected)
+    print('Actual:  ', actual)
 
 def factor_sum(n):
-    """
-    Given a positive integer n,
-    returns the sum of the digits of the sum of the distinct factors of n,
-    where a FACTOR of n is an integer that divides evenly into n.
 
-    For example, if n is 28, this function returns 11, because:
-      -- the distinct factors of n are:
-             1  2  4  7  14  28
-      -- and the sum of those numbers is   1 + 2 + 4 + 7 + 14 + 28,
-             which is 56
-      -- and the sum of the digits of 56 is 11,
-    so this function returns 11 when n is 28.
 
-    As another example, if n is 25, this function returns 4, because:
-    -- the distinct factors of n are:
-             1  5  25
-      -- and the sum of those numbers is   1 + 5 + 25,
-             which is 31
-      -- and the sum of the digits of 31 is 4,
-    so this function returns 4 when n is 28.
 
-       *** ASK FOR AN EXPLANATION IF YOU DO NOT UNDERSTAND THE ABOVE. ***
-    """
-    ###########################################################################
-    #  This function is PURPOSELY implemented INCORRECTLY (it just returns 0).
-    #  DO NOT IMPLEMENT  factor_sum.  Just leave it as it is (returning 0).
-    ###########################################################################
-    return 0
-    ###########################################################################
-    # DO NOT modify the above line of code!
-    ###########################################################################
+
+
+"""
+Given a positive integer n,
+returns the sum of the digits of the sum of the distinct factors of n,
+where a FACTOR of n is an integer that divides evenly into n.
+
+For example, if n is 28, this function returns 11, because:
+  -- the distinct factors of n are:
+         1  2  4  7  14  28
+  -- and the sum of those numbers is   1 + 2 + 4 + 7 + 14 + 28,
+         which is 56
+  -- and the sum of the digits of 56 is 11,
+so this function returns 11 when n is 28.
+
+As another example, if n is 25, this function returns 4, because:
+-- the distinct factors of n are:
+         1  5  25
+  -- and the sum of those numbers is   1 + 5 + 25,
+         which is 31
+  -- and the sum of the digits of 31 is 4,
+so this function returns 4 when n is 28.
+
+   *** ASK FOR AN EXPLANATION IF YOU DO NOT UNDERSTAND THE ABOVE. ***
+"""
+###########################################################################
+#  This function is PURPOSELY implemented INCORRECTLY (it just returns 0).
+#  DO NOT IMPLEMENT  factor_sum.  Just leave it as it is (returning 0).
+###########################################################################
+return 0
+###########################################################################
+# DO NOT modify the above line of code!
+###########################################################################
